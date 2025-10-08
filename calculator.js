d29f5d5634d0636340a24debc17dce34603c15f0
Rohit Chavda <rohitchavda@socialpilot.co>
2025-10-08 15:49:09 +0530

    feat: Implement multiply  function to pass test

--- calculator.js
+++ calculator.js
@@ -1,2 +1,7 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
+function multiply(a, b) {
+  return a * b;
+}
+
+export { multiply };
--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-import { add, subtract } from "./calculator.js";
+import { add, subtract, multiply } from "./calculator.js";
 
 describe("Calculator", () => {
   it("It should be true for Jest setup checks", () => {
