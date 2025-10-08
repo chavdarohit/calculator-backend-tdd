5d9cfda475ab64f22e3493e57f08c457fd1db38c
Rohit Chavda <rohitchavda@socialpilot.co>
2025-10-08 15:50:50 +0530

    feat: Implement multiply  function to pass test

--- calculator.js
+++ calculator.js
@@ -1,7 +1,3 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
-function multiply(a, b) {
-  return a * b;
-}
-
-export { multiply };
+export const multiply = (a, b) => a * b;
