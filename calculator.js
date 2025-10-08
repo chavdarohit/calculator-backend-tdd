aea00aca754d8e7d715e063d0823c081b17e4bc2
Rohit Chavda <rohitchavda@socialpilot.co>
2025-10-08 16:46:49 +0530

    feat: Implement add function to throw the error and pass the test when the string is passed in arguments

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
-export const add = (a, b) => a + b;
+export const add = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Invalid input it must be a number");
+  }
+  return a + b;
+};
 export const subtract = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
 export const divide = (a, b) => (b === 0 ? null : a / b);
