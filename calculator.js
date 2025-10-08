e24dd6c2e35469bd0c7c5cca1f9a189131cfae5d
Rohit Chavda <rohitchavda@socialpilot.co>
2025-10-08 16:38:38 +0530

    feat: Implement divide function to pass test

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
+function divide(a, b) {
+  if (b === 0) return null;
+  return a / b;
+}
+export { divide };
