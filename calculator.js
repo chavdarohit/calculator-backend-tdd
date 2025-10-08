bbadcdd60e4b306b381b922ffd86ca34bc644026
Rohit Chavda <rohitchavda@socialpilot.co>
2025-10-08 15:51:26 +0530

    Revert "feat: Implement multiply  function to pass test"
    This reverts commit 5d9cfda475ab64f22e3493e57f08c457fd1db38c.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
-export const multiply = (a, b) => a * b;
+function multiply(a, b) {
+  return a * b;
+}
+
+export { multiply };
