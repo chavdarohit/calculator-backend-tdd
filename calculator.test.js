cdb984444a08a1bc0ccbb45a551b3a99f825d057
Rohit Chavda <rohitchavda@socialpilot.co>
2025-10-08 15:35:29 +0530

    test: Add failing test for add function

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,10 @@
+import { add } from "./calculator.js";
+
 describe("Calculator", () => {
   it("It should be true for Jest setup checks", () => {
     expect(true).toBe(true);
+  });
+  test("should add two numbers correctly", () => {
+    expect(add(1, 2)).toBe(3);
   });
 });
